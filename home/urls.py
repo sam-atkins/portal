46ecd7a1be4945bbf0684afb3f6f8db2a3561690
Sam Atkins <samatkins@outlook.com>
2019-03-03 11:07:27 +0000

    feat: add index, login form and home pages

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.auth_login, name='auth_login'),
+    path('home', views.home_view, name='home_view'),
+]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect, render
 
 
 def index(request):
-    return render(request, 'home/base.html')
+    return render(request, 'home/index.html')
+
+
+# TODO(sam)
+# change form to username instead of email - check user Model requirements
+# hook up html form so posts on submit
+# verify logic and flow of the fn
+# add error message for unhappy path
+def auth_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return redirect('home')
+
+        else:
+            # No backend authenticated the credentials
+            # TODO(sam) re render form and error message
+            return render(request, 'home/login_form.html')
+
+    return render(request, 'home/login_form.html')
+
+
+# TODO(sam) add auth user decorator
+def home_view(request):
+    return render(request, 'home/home.html')
