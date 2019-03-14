4584e6f42e7e441d1d4837b4c93cd3131889a640
Sam Atkins <samatkins@outlook.com>
2019-03-14 07:21:43 +0000

    test: Add tests for index and home view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from users.models import CustomUser
 
-# Create your tests here.
+
+class HomeViewsLoggedInUserTestCase(TestCase):
+    def setUp(self):
+        self.client.force_login(
+            CustomUser.objects.get_or_create(username='testuser')[0])
+
+    def test_logged_in_user_index_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_logged_in_user_home_returns_200(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+
+class HomeViewsTestCase(TestCase):
+    def test_index_redirects_returns_302(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_index_redirect_follow_returns_200(self):
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_directs_to_login_view(self):
+        response = self.client.get('/')
+        self.assertRedirects(
+            response=response,
+            expected_url='/accounts/login/?next=/',
+            status_code=302,
+            target_status_code=200,
+            msg_prefix='',
+            fetch_redirect_response=True)
+
+    def test_home_redirects_returns_302(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_home_redirect_follow_returns_200(self):
+        response = self.client.get('/home/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_directs_to_login_view(self):
+        response = self.client.get('/home/')
+        self.assertRedirects(
+            response=response,
+            expected_url='/accounts/login/?next=/home/',
+            status_code=302,
+            target_status_code=200,
+            msg_prefix='',
+            fetch_redirect_response=True)
