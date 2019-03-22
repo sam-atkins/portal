c3609552e59b75b5effba4a84061ec2f0688d503
Sam Atkins <samatkins@outlook.com>
2019-03-22 20:11:09 +0000

    test: add further tests for auth

--- home/tests.py
+++ home/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from users.models import CustomUser
+from users.models import CustomUser, CustomUserManager
 
 
 class HomeViewsLoggedInUserTestCase(TestCase):
@@ -42,5 +42,16 @@ class HomeViewsTestCase(TestCase):
 
     def test_logged_out_user_home_renders_login_html(self):
         response = self.client.get('/', follow=True)
+        self.assertTemplateUsed(response, 'home/base.html',
+                                'registration/login.html')
+
+
+class LogInViewTestCase(TestCase):
+    def test_login_view_returns_200(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_renders_login_template(self):
+        response = self.client.get('/login/')
         self.assertTemplateUsed(response, 'home/base.html',
                                 'registration/login.html')
