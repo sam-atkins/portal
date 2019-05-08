be2d5140cb4fe33a284985c069abffb627375bb5
Sam Atkins <samatkins@outlook.com>
2019-05-08 06:51:01 +0100

    add weather for a default location to home view

--- home/views.py
+++ home/views.py
@@ -35,7 +35,26 @@ def logout_view(request):
 
 @login_required
 def home_view(request):
-    context = {}
+    context = {"weather": False, "weather_data": []}
+    location = get_config("default_weather_location")
+    if location is not None:
+        location_name = split_strings(original_string=location)
+        payload = {"name": location_name[0]}
+        service_proxy = ServiceProxy()
+        weather_data = service_proxy.service_request(
+            service_name="met_service",
+            service_version=1,
+            function_name="weather",
+            payload=payload,
+        )
+        if weather_data:
+            context["weather"] = True
+            location = get_config("default_location_name", "London")
+            parsed_weather_data = parse_weather_data(
+                location_name=location_name[1], weather_data=weather_data
+            )
+            weather_data = context.get("weather_data")
+            weather_data.append(parsed_weather_data)
     return render(request, "home/home.html", context=context)
 
 
